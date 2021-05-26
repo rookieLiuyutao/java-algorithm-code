57a3e3e5fa2c1900722c8b3b89acc1de55ce013d
liuyutaocode <958532148@qq.com>
2021-05-26 23:49:34 +0800

    默认的

--- src/leetcode1190反转每对括号间的子串/reverseParentheses2Class.java
+++ src/leetcode1190反转每对括号间的子串/reverseParentheses2Class.java
@@ -5,23 +5,23 @@ package leetcode1190反转每对括号间的子串;
  */
 public class reverseParentheses2Class {
     int idx = 0;
-    public StringBuffer dfs(String s){
+
+    public StringBuffer dfs(String s) {
         StringBuffer sb = new StringBuffer();
-        while (idx<sb.length()){
-            if (s.charAt(idx)!=')'){
+        while (idx < s.length()) {
+            if (s.charAt(idx) == '(') {
                 idx++;
                 sb.append(dfs(s).reverse());
-            }else if (s.charAt(idx)=='('){
+            } else if (s.charAt(idx) == ')') {
                 idx++;
                 return sb;
-            }else {
+            } else {
                 sb.append(s.charAt(idx));
+                idx++;
             }
         }
         return sb;
     }
-
-
 
     public String reverseParentheses(String s) {
         return dfs(s).toString();

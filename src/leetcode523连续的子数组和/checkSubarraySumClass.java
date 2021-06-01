d52d0658a85695ee05791b8dc9da955a751b1bef
liuyutaocode <958532148@qq.com>
2021-06-02 02:04:01 +0800

    默认的

--- src/leetcode523连续的子数组和/checkSubarraySumClass.java
+++ src/leetcode523连续的子数组和/checkSubarraySumClass.java
@@ -16,7 +16,7 @@ public class checkSubarraySumClass {
         //想要找到整除的，必须把余数为0的加进去，否则有两个这样的子数组才会返回true
         map.put(0,-1);
         int remainder = 0;
-//        当sum[i]-sum[j]为true时，sum[i]和sum[j]一定也都可以被k整除。因此只需要计算每个下标对应的前缀和除以 k 的余数即可，使用哈希表存储每个余数第一次出现的下标。
+//        当sum[i]-sum[j]为true时，sum[i]和sum[j]除以k的余数必定相同。因此只需要计算每个下标对应的前缀和除以 k 的余数即可，使用哈希表存储每个余数第一次出现的下标。
 
 
         for(int i = 0; i <n; i++) {
